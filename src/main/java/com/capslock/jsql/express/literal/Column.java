978ae85610ac778acd020738a5be100dc339703d
alvin <alvin@liao.com>
2016-08-05 16:50:25 +0800

    refactoring

--- src/main/java/com/capslock/jsql/express/literal/Column.java
+++ src/main/java/com/capslock/jsql/express/literal/Column.java
@@ -1,10 +1,21 @@
 package com.capslock.jsql.express.literal;
 
+import com.capslock.jsql.express.operator.Order;
+import com.capslock.jsql.express.query.select.order.OrderExpress;
+
 /**
  * Created by capslock.
  */
 public class Column extends StringLiteral {
     public Column(final String value) {
         super("`" + value + "`");
+    }
+
+    public OrderExpress orderByAsc() {
+        return new OrderExpress(this, Order.asc);
+    }
+
+    public OrderExpress orderByDesc() {
+        return new OrderExpress(this, Order.desc);
     }
 }
